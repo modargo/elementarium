d78d4b009ef3e581a50e8c8bc228cd5754109f15
modargo <modargo@gmail.com>
2021-01-01 15:32:22 -0500

    Slightly reduce the damage gained from upgrading the Fire and Ice blades

--- src/main/java/elementarium/cards/elementalblades/FireblessedBlade.java
+++ src/main/java/elementarium/cards/elementalblades/FireblessedBlade.java
@@ -25,7 +25,7 @@ public class FireblessedBlade extends CustomCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
     private static final int BASE_DAMAGE = 12;
-    private static final int UPGRADE_DAMAGE = 7;
+    private static final int UPGRADE_DAMAGE = 6;
     private static final int STAT_AMOUNT = 1;
     private static final int UPGRADE_STAT_AMOUNT = 1;
 
