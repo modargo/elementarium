ba9107b0d7938ba2858810f37755689291b0253d
modargo <modargo@gmail.com>
2022-03-20 13:57:21 -0400

    Increase War Golem HP

--- src/main/java/elementarium/monsters/elites/WarGolem.java
+++ src/main/java/elementarium/monsters/elites/WarGolem.java
@@ -42,10 +42,10 @@ public class WarGolem extends CustomMonster
     private static final int A18_FOCUS_AMOUNT = 1;
     private static final int METALLICIZE = 6;
     private static final int A8_METALLICIZE = 9;
-    private static final int HP_MIN = 93;
-    private static final int HP_MAX = 95;
-    private static final int A8_HP_MIN = 97;
-    private static final int A8_HP_MAX = 99;
+    private static final int HP_MIN = 102;
+    private static final int HP_MAX = 104;
+    private static final int A8_HP_MIN = 106;
+    private static final int A8_HP_MAX = 108;
     private int vulnerableDamage;
     private int frailDamage;
     private int dexDamage;
