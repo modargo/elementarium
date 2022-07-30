7126703a9e2126a4c59b9292512218c9469ea302
modargo <modargo@gmail.com>
2022-07-30 07:46:28 -0400

    Fix Sticky Tar being removed after non-elite fights that include elite enemies, such as the Sentry and Spheric Guardian fight

--- src/main/java/elementarium/subscribers/RemoveStickyTarPostEliteBattleSubscriber.java
+++ src/main/java/elementarium/subscribers/RemoveStickyTarPostEliteBattleSubscriber.java
@@ -3,7 +3,6 @@ package elementarium.subscribers;
 import basemod.interfaces.PostBattleSubscriber;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.vfx.cardManip.PurgeCardEffect;
 import elementarium.cards.tar.StickyTar;
@@ -11,10 +10,10 @@ import elementarium.cards.tar.StickyTar;
 public class RemoveStickyTarPostEliteBattleSubscriber implements PostBattleSubscriber {
     @Override
     public void receivePostBattle(AbstractRoom abstractRoom) {
-        boolean isElite = abstractRoom.eliteTrigger || (abstractRoom.monsters != null
-               && abstractRoom.monsters.monsters != null
-               && abstractRoom.monsters.areMonstersDead() //No credit if you smoke bomb out
-               && abstractRoom.monsters.monsters.stream().anyMatch(m -> m.type == AbstractMonster.EnemyType.ELITE));
+        boolean isElite = abstractRoom.monsters != null
+                && abstractRoom.monsters.monsters != null
+                && abstractRoom.monsters.areMonstersDead() //No credit if you smoke bomb out
+                && abstractRoom.eliteTrigger;
         if (isElite) {
             AbstractCard stickyTarCard = AbstractDungeon.player.masterDeck.findCardById(StickyTar.ID);
             if (stickyTarCard != null ){
