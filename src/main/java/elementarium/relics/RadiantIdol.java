1c55b2cdc3fbfc7c96ee0024e048adfb62b9099e
modargo <modargo@gmail.com>
2022-08-01 13:49:57 -0400

    Add relic stats for Radiant Idol

--- src/main/java/elementarium/relics/RadiantIdol.java
+++ src/main/java/elementarium/relics/RadiantIdol.java
@@ -2,6 +2,9 @@ package elementarium.relics;
 
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
@@ -9,6 +12,10 @@ import elementarium.Elementarium;
 import elementarium.util.TextureLoader;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RadiantIdol extends CustomRelic {
     public static final String ID = "Elementarium:RadiantIdol";
@@ -16,12 +23,16 @@ public class RadiantIdol extends CustomRelic {
     private static final Texture OUTLINE = TextureLoader.getTexture(Elementarium.relicOutlineImage(ID));
     private static final int HEAL_AMOUNT = 6;
 
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String HEAL_STAT = "heal";
+
     public RadiantIdol() {
         super(ID, IMG, OUTLINE, RelicTier.SPECIAL, LandingSound.HEAVY);
     }
 
     @Override
     public void onObtainCard(AbstractCard c) {
+        incrementHealStat();
         AbstractDungeon.player.heal(HEAL_AMOUNT);
     }
 
@@ -32,5 +43,37 @@ public class RadiantIdol extends CustomRelic {
 
     public AbstractRelic makeCopy() {
         return new RadiantIdol();
+    }
+
+    public String getStatsDescription() {
+        return MessageFormat.format(DESCRIPTIONS[1], stats.get(HEAL_STAT));
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        return getStatsDescription();
+    }
+
+    public void resetStats() {
+        stats.put(HEAL_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(HEAL_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(HEAL_STAT, jsonArray.get(0).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementHealStat() {
+        stats.put(HEAL_STAT, stats.getOrDefault(HEAL_STAT, 0) + HEAL_AMOUNT);
     }
 }
